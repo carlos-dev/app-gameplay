01fddfe88503178bcf18869ba5bc7989bf415260
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2021-09-09 00:54:38 -0300

    criando splash screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+import { styles } from './styles';
+
+export default function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>oiii</Text>
+
+      <TextInput style={styles.input} />
+    </View>
+  );
+}
